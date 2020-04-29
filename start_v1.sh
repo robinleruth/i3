efc8f48e6d94ae5ac68832ccb433fabc373c5297
robinleruth <robin.leruth@gmail.com>
2020-04-29 00:31:49 -0400

    new conf

--- start_v1.sh
+++ start_v1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-i3-msg -q "workspace 1; append_layout ~/.config/i3/workspace_1.json"
+i3-msg -q "append_layout ~/.config/i3/workspace_1.json"
 
-(vlc &)
-(gnome-terminal &)
 (gnome-terminal &)
